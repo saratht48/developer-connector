658f3cf2ff82f60d8af13de8a79de7e3a05a7028
saratht48 <saratht199748@gmail.com>
2024-06-02 22:49:25 +0530

    fixed issues

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express=require('express')
-const { register, login, addProfile } = require('../controllers/userController')
+const { register, login, addProfile,getAllProfiles } = require('../controllers/userController')
 const { protect } = require('../middleware/auth')
 const userRouter=express.Router()
 userRouter.route('/register').post(register)
 userRouter.route('/login').post(login)
 userRouter.route('/updateProfile').post(protect,addProfile)
+userRouter.route('/getAllProfiles').get(getAllProfiles)
+
 module.exports=userRouter
